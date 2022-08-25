7aa3b58f3bc64e7683ae243d4d1e6883cd47e84c
Nguyen Quoc Khanh <98071179+QuocKhanh1920@users.noreply.github.com>
2022-08-25 09:12:09 +0700

    Add files via upload

---
+++ Magento2/HelloWorld/Model/ResourceModel/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Magento2\HelloWorld\Model\ResourceModel;
+
+class Post extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+{
+	
+	public function __construct(
+		\Magento\Framework\Model\ResourceModel\Db\Context $context
+	)
+	{
+		parent::__construct($context);
+	}
+	
+	protected function _construct()
+	{
+		$this->_init('magento2_helloworld_post', 'id');
+	}
+	
+}
